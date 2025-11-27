f4a587ccf7beb06456b5bb550d014cea46bf80ce
Jilin <>
2025-11-27 13:57:40 +0800

    34

--- kPro/NoteEditViewController.swift
+++ kPro/NoteEditViewController.swift
@@ -109,7 +109,7 @@ class NoteEditViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         saveNote()
-        print("2")
+        print("234")
     }
     
     // MARK: - Setup
