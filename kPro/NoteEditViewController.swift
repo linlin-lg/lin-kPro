3891d18c0a10deb1104a8c6d0825a2fba64cfd63
Jilin <>
2025-11-27 13:36:49 +0800

    commit-2

--- kPro/NoteEditViewController.swift
+++ kPro/NoteEditViewController.swift
@@ -17,7 +17,7 @@ class NoteEditViewController: UIViewController {
     // MARK: - UI Components
     private lazy var titleTextField: UITextField = {
         let textField = UITextField()
-        textField.placeholder = "标题1"
+        textField.placeholder = "标题2"
         textField.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         textField.borderStyle = .none
         textField.backgroundColor = .clear
