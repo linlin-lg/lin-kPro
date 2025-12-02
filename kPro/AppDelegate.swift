0988c6ad4bdd41511d81d73a0273aa30d15fc7d1
Jilin <>
2025-12-02 15:52:11 +0800

    p1

--- kPro/AppDelegate.swift
+++ kPro/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setupNavigationBarAppearance()
+        print("1")
         return true
     }
 
--- kPro/NoteEditViewController.swift
+++ kPro/NoteEditViewController.swift
@@ -108,8 +108,6 @@ class NoteEditViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         saveNote()
-        print("234")
-
     }
     
     // MARK: - Setup
